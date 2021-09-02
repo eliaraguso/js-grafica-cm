38e14484fa6ae7c1092d63184a7de19433d2514a
eliaraguso <elia.raguso@gmail.com>
2021-09-02 10:45:33 +0200

    creta funzione di verifica della presenza o meno di un elemento in un array

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,34 @@
 // Tramite una funzione javascript disegnare in pagina la griglia con massimo 10 celle per riga.
 // Al click su una cella dovrà essere mostrato con un alert il numero della cella e il suo background diventerà rosso.
 
+// ----------------FUNZIONI-----------------
+
+// Funzione per creare la griglia di gioco
+function creaGriglia(numCelle, idCampo){
+    for (let i = 1; i <= numCelle; i++) {
+    document.getElementById(idCampo).innerHTML += `<div class="quadrato">${i}</div>`;
+    }
+}
+
+// Funzione per verificare, dato l'array e l'elemento da cercare, se l'elemento è presente = true altrimenti false
+function inArray(arr, el) {
+    
+    let count = 0;
+    let trovato = false;
+    
+    while(count < arr.lenght && trovato == false) {
+        if (arr[count] == el){
+            return true;
+        }
+
+        count++;
+
+    }
+
+    return falso;
+}
+
+// ---------------PROGRAMMA PRINCIPALE-----------
 
 // Chiedo all'utente di inserire il numero di celle di cui sarà composto il campo da gioco.
 let totaleCelle = parseInt(prompt("Inserisci il numero di celle che andranno a formare il campo da gioco in modo che sia sempre una scacchiera quadrata. Il valore dev'essere compreso tra 4 e 100 e gli unici valori consentiti corrispondano al quadrato del numero di celle o colonne di cui sarà composto il campo (4, 9, 16, 25, 36, 49, 64, 81, 100)"));
@@ -38,13 +66,7 @@ while (isNaN(totaleCelle) || totaleCelle < 4 || totaleCelle >100 ) {
 
 
 
-// Creo la funzione per creare la il campo da gioco
 
-function creaGriglia(numCelle){
-    for (let i = 1; i <= numCelle; i++) {
-    document.getElementById(idCampo).innerHTML += `<div class="quadrato">${i}</div>`;
-    }
-}
 
 
 // faccio si che all'evento click sopra ogni cella, una funzione che creo, faccia cambiare colore alla cella e riporti un alert col numero della cella selezionata
@@ -56,8 +78,5 @@ document.getElementById(idCampo).addEventListener("click",
 )
 
 // Scateno la funzione
-creaGriglia(totaleCelle);
-
-
-
+creaGriglia(totaleCelle, "campo");
 
